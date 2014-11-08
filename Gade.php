4c3513c0ed6f0495a67bd82e747fae14d15169f7
Chau Duong <chau.duong1995@gmail.com>
2014-11-08 14:05:46 -0800

    Version 2
    This is the second version of Aviation Project.

---
+++ Gade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+
+<body>
+    
+		<h1>Final Quiz </h1>
+	<?php	
+            
+            question-1-answers = $answer1;
+           
+        
+            $totalCorrect = 0;
+            
+            if ($answer1 == 'B') { $totalCorrect++; }
+            if ($answer2 == 'A') { $totalCorrect++; }
+            if ($answer3 == 'C'){ $totalCorrect++; }
+           
+            
+            echo <div id="results">Total score: 3/3</div>
+               
+    
+
+
+</body>
+</html>
