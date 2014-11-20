791eeafb9c484aee4e002b745881ed91e6110271
Chau Duong <chau.duong1995@gmail.com>
2014-11-20 00:05:11 -0800

    Version 3
    This is the version 3 of Group 3's Aviation project

--- Gade.php
+++ Gade.php
@@ -7,22 +7,40 @@
 
 <body>
     
+
+
 		<h1>Final Quiz </h1>
-	<?php	
+		
+     <?php
             
-            question-1-answers = $answer1;
-           
-        
-            $totalCorrect = 0;
             
-            if ($answer1 == 'B') { $totalCorrect++; }
-            if ($answer2 == 'A') { $totalCorrect++; }
-            if ($answer3 == 'C'){ $totalCorrect++; }
-           
-            
-            echo <div id="results">Total score: 3/3</div>
-               
+    $answer1 = $_POST['question-1-answers'];
+    $answer2 = $_POST['question-2-answers'];
+    $answer3 = $_POST['question-3-answers'];
+    $answer4 = $_POST['question-4-answers'];
+    $answer5 = $_POST['question-5-answers'];
+    $answer6 = $_POST['question-6-answers'];
+    $answer7 = $_POST['question-7-answers'];
+   
+
+
+    $score = 0;
     
+    if ($answer1 == "B") {$score++;}
+    if ($answer2 == "A") { $score++; }
+    if ($answer3 == "C") { $score++; }
+    if ($answer4 == "A") { $score++; }
+    if ($answer5 == "A") { $score++; }
+    if ($answer6 == "C") { $score++; }
+    if ($answer7 == "D") { $score++; }
+
+   echo "<h2> TOTAL SCORE = $score / 7 <h2>"
+    
+ 
+     ?>
+	
+	
+
 
 
 </body>
